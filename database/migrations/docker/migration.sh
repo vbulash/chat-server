b70f2176cbc259e603f46703c5e4283565285c98
Валерий Булаш <v.bulash@ansgar.ru>
2024-10-20 14:38:53 +0300

    Миграции в докере-2

--- database/migrations/docker/migration.sh
+++ database/migrations/docker/migration.sh
@@ -2,4 +2,4 @@
 source .env
 
 export MIGRATION_DSN="host=postgres port=5432 dbname=$DB_DATABASE user=$DB_USER password=$DB_PASSWORD sslmode=disable"
-bin/goose -dir . postgres "$MIGRATION_DSN" up -v
+goose -dir . postgres "$MIGRATION_DSN" up -v
